04625eb609331007ee747f43ad3d90d516da3af8
azbury <acasbury25@aol.com>
2020-07-27 20:39:35 -0400

    cards container file

---
+++ src/containers/CardsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class CardsContainer extends Component {
+
+    render() {
+      return (
+        <div>
+          <Cards />
+        </div>
+      )
+    }
+}
+
+export default CardsContainer
