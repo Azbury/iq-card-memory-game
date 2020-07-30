308664e5e19bbb90b64c4acaeedb31611c4dea04
azbury <acasbury25@aol.com>
2020-07-30 13:05:24 -0400

    adding cat and dog pic

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import ReactCardFlip from 'react-card-flip';
+import Dog from '../dog.png'
+import Cat from '../cat.png'
 
 class FlipCard extends Component {
  constructor() {
@@ -19,12 +21,12 @@ class FlipCard extends Component {
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
           <div>
-            <img src="https://unsplash.com/photos/8Myh76_3M2U"></img>
+            <img className="dog-pic" src={Dog}></img>
           <button onClick={this.handleClick}>Click to flip</button>
           </div>
  
           <div>
-          back
+            <img className="cat-pic" src={Cat}></img>
           <button onClick={this.handleClick}>Click to flip</button>
           </div>
       </ReactCardFlip>
