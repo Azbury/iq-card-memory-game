557cd4ec34c4759dc5ff9f9d2b63b36177d17c93
azbury <acasbury25@aol.com>
2020-08-01 19:06:37 -0400

    setting up start game button how it will work

--- src/components/StartGameInput.js
+++ src/components/StartGameInput.js
@@ -5,8 +5,15 @@ class StartGameInput extends Component {
     render () {
         return (
             <div>
-
+                <form>
+                    
+                </form>
             </div>
         )
     }
 }
+
+//button to start the game, this will have a handleSubmit function that will load the cards to the state.
+//the cards will be put together and shuffled in here and then sent to the state to be rendered in that order.
+//that way each time the cards are rendered they are rendered in the right order because the order is coming from the store
+//each time
