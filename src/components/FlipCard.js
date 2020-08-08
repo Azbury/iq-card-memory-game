17311faeb5668165b3c22860d7805e9fefc7ec3e
Azbury <acasbury25@aol.com>
2020-08-08 12:50:19 -0400

    cleaning up code

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -5,29 +5,19 @@ import SmileyFace from '../pictures/smileyface.png'
 const Animal = require.context('./animals', true)
 
 class FlipCard extends Component {
-  //the constructor is only called at the creation of the component so i can not use that to update the state from the props of Cards.js
-  constructor(props) {
-    super(props);
-      this.state = {
-      isFlipped: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
 
   handleClick(e) {
     e.preventDefault()
     this.props.flipCard(this.props.id)
     setTimeout(() => this.props.isMatch(this.props.animal), 2000)
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped}));
   }
-  //next step is to make it so when to cards match they disappear now, just take them out of the state when 2 cards match now
 
   render() {
     return (
       <ReactCardFlip isFlipped={this.props.isFlipped} flipDirection="vertical">
           <div>
             <img className="dog-pic" src={QuestionMark} alt={'question mark'}></img>
-          <button onClick={this.handleClick}>Click to flip</button>
+          <button onClick={(event) => this.handleClick(event)}>Click to flip</button>
           </div>
  
           <div className="back-card">
