1e91c376be7da1624c9ee7dd83eea2456447c986
Azbury <acasbury25@aol.com>
2020-08-09 11:23:41 -0400

    changing dispatch name

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -11,7 +11,7 @@ class CardsContainer extends Component {
         return (
             <div className='final-container'>
               <h1>IQ Card Memory Game</h1>
-              <SignupInput addUser={this.props.addUser} newUser={this.props.newUserButton}/>
+              <SignupInput addUser={this.props.addUser} newUser={this.props.newUser}/>
               {this.props.submitted && <User currentUser={this.props.users.find(user => user.id === this.props.currentUser)}/>}
               {this.props.submitted && <StartGameInput addCards={this.props.addCards}/>}
               {this.props.gameStarted && <Cards isMatch={this.props.isMatch} currentCards={this.props.currentCards} flipCards={this.props.flipCards} flipCard={this.props.flipCard}/>}
@@ -24,7 +24,7 @@ const mapStateToProps = state => ({ currentUser: state.currentUser, submitted: s
 
 const mapDispatchToProps = dispatch => ({
   addUser: username => dispatch({type: 'ADD_USER', username}),
-  newUserButton: username => dispatch({type: 'NEW_USER_BUTTON', username}),
+  newUser: username => dispatch({type: 'NEW_USER_BUTTON', username}),
   isMatch: imageid => dispatch({type: 'IS_MATCH', imageid}),
   addCards: cards => dispatch({type: 'ADD_CARDS', cards}),
   flipCards: frontSideUp => dispatch({type: 'FLIP_CARDS', frontSideUp}),
