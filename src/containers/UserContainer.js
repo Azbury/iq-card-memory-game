73846b34ccbe4098ae5ceb0e55c05990a9e74e4c
azbury <acasbury25@aol.com>
2020-08-18 14:50:32 -0400

    creating user container

---
+++ src/containers/UserContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UserContainer extends Component {
+    render () {
+        return (
+            
+        )
+    }
+}
+
+export default UserContainer
