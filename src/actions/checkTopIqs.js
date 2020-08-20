8224d64bafda6aa747bf4868a3c0ad09664714af
azbury <acasbury25@aol.com>
2020-08-19 20:06:25 -0400

    check top iqs action

---
+++ src/actions/checkTopIqs.js
@@ -0,0 +1,8 @@
+export function checkTopIqs(user_id) {
+    return (dispatch) => {
+      dispatch({ type: 'START_CHECKING_TOP_IQS_REQUEST' });
+      fetch(`http://localhost:3000/users/${user_id}`)
+        .then(response => response.json())
+        .then(iqs => dispatch({ type: 'CHECKING_TOP_IQS', iqs }));
+    }
+}
