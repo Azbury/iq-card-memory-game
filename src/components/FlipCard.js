1ff30ec29b0cb223f7f463287724b6d998ec6253
azbury <acasbury25@aol.com>
2020-08-30 18:54:41 -0400

    adding comments

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -4,11 +4,14 @@ import QuestionMark from '../pictures/questionmark.png'
 import SmileyFace from '../pictures/smileyface.png'
 const Animal = require.context('./animals', true)
 
+//FlipCard
+//Holds pictures for cards as well as the ability flip the card over. Call isMatch after a 2 second timeout to check if 2 cards match when they are flipped.
 class FlipCard extends Component {
 
   handleClick(e) {
     e.preventDefault()
     this.props.flipCard(this.props.id)
+    //Has a 2 second delay so that a user has a chance to see the cards if they match incorrectly.
     setTimeout(() => this.props.isMatch(this.props.animal), 2000)
   }
 
