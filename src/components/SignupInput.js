fd53e8f54962931c2c82c8b69c7539b0bc04b8e8
azbury <acasbury25@aol.com>
2020-09-01 18:38:14 -0400

    changing formatting

--- src/components/SignupInput.js
+++ src/components/SignupInput.js
@@ -1,52 +1,42 @@
 import React, { Component } from 'react'
 
+//SignupInput
+//Holds the form for a user to sign up with their username
 class SignupInput extends Component {
-  state = {
-    username: '',
-    newUserButton: ''
-  }
-
-  handleOnChange(event) {
-    this.setState({
-      username: event.target.value,
-    });
-  }
-
-  handleOnSubmit(event) {
-    event.preventDefault()
-    this.props.addUser(this.state.username)
-    this.setState({
+    state = {
       username: '',
-      newUserButton: 'active'
-    })
-  }
+    }
 
-  handleClick(event) {
-    event.preventDefault()
-    this.props.newUser(this.state.username)
-    this.setState({
-      username: '',
-      newUserButton: ''
-    })
-  }
+    handleOnChange(event) {
+      this.setState({
+        username: event.target.value,
+      })
+    }
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <p className="user-form">Please Enter Your Username</p>
-          <input
-            type="text"
-            value={this.state.username}
-            onChange={(event) => this.handleOnChange(event)} />
-          <br></br>
-          <br></br>
-          <input className="button" type="submit" />
-        </form>
-        <button className={this.state.newUserButton === "active" ? "" : "hidden"} onClick={(event) => this.handleClick(event)}>New User</button>
-      </div>
-    );
-  }
-};
+    handleOnSubmit(event) {
+      event.preventDefault()
+      this.props.addUser(this.state.username)
+      this.setState({
+        username: ''
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <form onSubmit={(event) => this.handleOnSubmit(event)}>
+            <p className="user-form">Please Enter Your Username</p>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={(event) => this.handleOnChange(event)} />
+            <br></br>
+            <br></br>
+            <input className="button" type="submit" />
+          </form>
+        </div>
+      )
+    }
+}
 
 export default SignupInput;
