4bfb2f5f89f7923add02662d932af7ec8b3d22e7
azbury <acasbury25@aol.com>
2020-09-04 09:01:18 -0400

    removing unused statetoprops

--- src/containers/LeaderboardContainer.js
+++ src/containers/LeaderboardContainer.js
@@ -2,29 +2,36 @@ import React, { Component } from 'react'
 import ProfilePageButton from '../components/ProfilePageButton'
 import { connect } from 'react-redux'
 
+//LeaderboardContainer
+//Holds all the components for the leaderboard page
 class LeaderboardContainer extends Component {
 
     state = {
         users: []
     }
-
+    
+    //compare method used in sort method to sort leaderboard in decending order by a users top iq score
     compare(a, b) {
         const topIqA = a.iqs.map(iq => iq.iq).sort(function(a, b){return b-a})[0]
         const topIqB = b.iqs.map(iq => iq.iq).sort(function(a, b){return b-a})[0]
       
-        let comparison = 0;
+        let comp = 0;
         if (topIqA > topIqB) {
-          comparison = 1;
+          comp = 1;
         } else if (topIqA < topIqB) {
-          comparison = -1;
+          comp = -1;
         }
-        return comparison * -1;
+        return comp * -1;
     }
 
     render () {
-        let userArray = this.state.users.sort(this.compare).map(user => 
-                        <div><div className="leaderboard-user"><div className="leaderboard-username">{user.username}</div><br></br>Top IQ Score<div>{user.iqs.map(iq => iq.iq).sort(function(a, b){return b-a})[0]}</div></div><br></br></div>
-                                            )
+        //sorted user array
+        let userArray = this.state.users
+                        .sort(this.compare)
+                        .map(user => 
+                        <div><div className="leaderboard-user"><div className="leaderboard-username">{user.username}</div><br></br>Top IQ Score<div>{user.iqs.map(iq => iq.iq)
+                        .sort(function(a, b){return b-a})[0]}</div></div><br></br></div>
+                            )
 
         return (
             <div className="leaderboard-container">
@@ -37,6 +44,7 @@ class LeaderboardContainer extends Component {
         )
     }
 
+    //API call to load up all users
     componentDidMount() {
         fetch('http://localhost:3000/users')
           .then(response => response.json())
@@ -48,10 +56,8 @@ class LeaderboardContainer extends Component {
     }
 }
 
-const mapStateToProps = state => ({ currentUser: state.currentUser, userSubmitted: state.userSubmitted, currentCards: state.currentCards, iqs: state.userTopIqs})
-
 const mapDispatchToProps = dispatch => ({
     stopGame: () => dispatch({type: 'STOP_GAME'})
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(LeaderboardContainer)
+export default connect(null, mapDispatchToProps)(LeaderboardContainer)
