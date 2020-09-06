f5e68384d4cfbb4a849a65b3b6aed4560ee95132
azbury <acasbury25@aol.com>
2020-09-05 20:01:26 -0400

    adding seed data

--- iq-card-memory-game-api/db/seeds.rb
+++ iq-card-memory-game-api/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'The Best', iqs: [Iq.create(iq: '100')])
+User.create(username: 'The Worst', iqs: [Iq.create(iq: '50')])
+User.create(username: 'Woohoo', iqs: [Iq.create(iq: '120')])
+User.create(username: 'TeeHee', iqs: [Iq.create(iq: '80')])
+User.create(username: 'Woza', iqs: [Iq.create(iq: '190')])
